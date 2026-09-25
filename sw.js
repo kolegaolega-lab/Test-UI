b98d28b45b20975adc7062b31dd20f993954a7a2
kolegaolega-lab <kolegaolega@gmail.com>
2026-09-26 01:03:58 +0300

    Update service worker version to v14

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-const SW_VERSION = 'v13';
+const SW_VERSION = 'v14';
 const CACHE_SHELL = `fb-shell-${SW_VERSION}`;
 const CACHE_RUNTIME = `fb-runtime-${SW_VERSION}`;
 const CACHE_MAX_ENTRIES = 80;
